9d9a632625de543caa57d13764e134618aaa3089
Colin Howald <cjhowald@gmail.com>
2025-09-14 15:33:34 -0700

    make entire row clickable

--- src/components/download-manager/download-manager-row.tsx
+++ src/components/download-manager/download-manager-row.tsx
@@ -14,19 +14,30 @@ export const DownloadManagerRow = ({
   onSelect,
 }: DownloadManagerRowProps) => {
   const disableSelect = file.status !== "available";
+  const rowID = `row-${id}`;
   return (
-    <tr className="border-b border-gray-300 p-2" id={id}>
+    <tr
+      className="border-b border-gray-300 p-2 cursor-pointer"
+      onClick={() => onSelect(!selected)}
+      id={rowID}
+    >
       <td className="m-x-4 p-2">
         <input
           id={id}
           disabled={disableSelect}
           checked={disableSelect ? false : selected}
+          aria-labelledby={rowID}
           type="checkbox"
-          onChange={(e) => onSelect(!!e.target.checked)}
+          onChange={() => onSelect(!selected)}
         />
       </td>
-      <td className="p-2">{file.name}</td>
-      <td className="p-2">{file.device}</td>
+      <td className="p-2">
+        <label htmlFor={id}></label>
+        {file.name}
+      </td>
+      <td className="p-2">
+        <label htmlFor={id}>{file.device}</label>
+      </td>
       <td className="p-2">{file.path}</td>
       <td className="p-2">{file.status}</td>
     </tr>
