145d6b699594b9e82ab82e0a6ab65eb183e2d136
Colin Howald <cjhowald@gmail.com>
2025-09-14 15:55:03 -0700

    select all/none

--- src/components/download-manager/download-manager.tsx
+++ src/components/download-manager/download-manager.tsx
@@ -13,24 +13,56 @@ const getFileID = (file: DownloadFile) => `${file.device}-${file.path}`;
 export const DownloadManager = ({ files }: DownloadManagerProps) => {
   const [selectedRows, setSelectedRows] = useState<Set<string>>(new Set());
 
+  const allRowsSelected = selectedRows.size === files.length;
+
   const handleSelectChange = (id: string, selected: boolean) => {
-    const updatedSelected = new Set(selectedRows);
     if (selected) {
-      updatedSelected.add(id);
+      selectRow(id);
     } else {
-      updatedSelected.delete(id);
+      unSelectRow(id);
     }
+  };
+
+  const selectRow = (id: string) => {
+    const updatedSelected = new Set(selectedRows);
+    updatedSelected.add(id);
     setSelectedRows(updatedSelected);
+  };
+
+  const unSelectRow = (id: string) => {
+    const updatedSelected = new Set(selectedRows);
+    updatedSelected.delete(id);
+    setSelectedRows(updatedSelected);
+  };
+
+  const selectAll = () => {
+    const IDs = files.map(getFileID);
+    setSelectedRows(new Set([...IDs]));
+  };
+
+  const unSelectAll = () => setSelectedRows(new Set());
+
+  const handleSelectAllClicked = () => {
+    if (!allRowsSelected) {
+      selectAll();
+    } else {
+      unSelectAll();
+    }
   };
 
   return (
     <div className="border border-gray-300 w-full">
       <div className="flex items-center border-b border-gray-300">
-        <input className="m-2" type="checkbox" />
+        <input
+          className="m-2"
+          type="checkbox"
+          checked={allRowsSelected ? true : false}
+          onClick={handleSelectAllClicked}
+        />
         <span className="m-2">Selected 2</span>
         <button className="m-2">Download Selected</button>
       </div>
-      <table className="    text-left border-collapse w-full">
+      <table className="text-left border-collapse w-full">
         <thead className="border-b border-gray-300">
           <tr>
             <th className="p-2"></th>
